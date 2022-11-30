63599ee3707b6416d62357b4aced16c65185e750
Thomas CHARMES <charmes.thomas@gmail.com>
2022-11-30 15:41:54 +0100

    link existing project

---
+++ src/main/java/com/charmest/webapp/CustomProperties.java
@@ -0,0 +1,23 @@
+package com.charmest.webapp;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Configuration
+@ConfigurationProperties(prefix = "com.charmest.webapp")
+@Data
+public class CustomProperties {
+	
+	private String apiUrl;
+
+	public String getApiUrl() {
+		return apiUrl;
+	}
+
+	public void setApiUrl(String apiUrl) {
+		this.apiUrl = apiUrl;
+	}
+
+}
---
+++ src/main/java/com/charmest/webapp/model/Employee.java
@@ -0,0 +1,58 @@
+package com.charmest.webapp.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+	private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String mail;
+
+    private String password;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
